8d7ea0b5a87e93530388f1f77a42f1df5261090e
Nathan Steed <steedn@outlook.com>
2020-07-30 22:04:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,12 @@
+require "pry"
+
 def my_each(array) # put argument(s) here
   i = 0
   while array.length > 0
     yield(array)
     i +=1
     break if i > array.length
+    binding.pry
   end
   # code here
 end
