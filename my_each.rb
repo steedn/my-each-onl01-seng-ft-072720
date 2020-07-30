ed211105d83553b5627af9c8cf49358020a970a5
Nathan Steed <steedn@outlook.com>
2020-07-30 22:09:36 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+require "pry"
+
+def my_each(array) # put argument(s) here
+  i = 0
+  while array.length > 0
+    yield(array[i])
+    i +=1
+    break if i >= array.length
+    # binding.pry
+  end
+  array
   # code here
 end
